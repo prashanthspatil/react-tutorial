3bccf579ec3605d950801edc06d0452956e1d04c
Prashanth <Prashanth@shoptreeapp.com>
2020-07-14 21:38:26 +0530

    Updates

---
+++ precache-manifest.6ea5453b59fe891a35a268afd36e9f8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbe994b0bfbc44e52e5a4e1cb6971565",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "bdffba2b703ef1a8a059",
+    "url": "/react-tutorial/static/css/main.9447384d.chunk.css"
+  },
+  {
+    "revision": "9d2ae3c2628a4c5fd0d4",
+    "url": "/react-tutorial/static/js/2.67691ccd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.67691ccd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdffba2b703ef1a8a059",
+    "url": "/react-tutorial/static/js/main.b8c17f32.chunk.js"
+  },
+  {
+    "revision": "d2980c81d16204d1adff",
+    "url": "/react-tutorial/static/js/runtime-main.fba2e450.js"
+  }
+]);
